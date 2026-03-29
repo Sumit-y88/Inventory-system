d8eddb9cc219e1bd0c1b619f4b0ff82f3cecde96
Sumit-y88 <sy687099@gmail.com>
2026-03-29 15:57:44 +0530

    made some important changes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";  
+import authRoutes from "./routes/authRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: "https://your-frontend-url",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
+
 app.use(express.json());
 
 connectDB();
@@ -30,15 +25,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 
-const frontendDistPath = path.resolve(__dirname, "../frontend/dist");
-app.use(express.static(frontendDistPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendDistPath, "index.html"));
-});
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

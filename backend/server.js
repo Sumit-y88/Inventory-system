16d4455771eafc73f6dc2f71ef0d6b9e9dae9ac3
Sumit-y88 <sy687099@gmail.com>
2026-03-29 17:10:23 +0530

    last baar kar raha hu

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true
 }));
 
@@ -18,12 +24,22 @@ app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
+
+app.get("/api/health", (req, res) => {
   res.send("Inventory API running");
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/items", itemRoutes);
+// exact frontend dist path
+const frontendPath = path.resolve(__dirname, "../frontend/dist");
+console.log("Frontend Path:", frontendPath);
+
+app.use(express.static(frontendPath));
+
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
